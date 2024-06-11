8ccdfbf275b37295c0751d2655457089bdf1fc37
신선한 노래 <racimo.715@gmail.com>
2024-06-11 15:16:45 +0900

    리액트로 만든 쇼핑몰 메인

---
+++ src/components/WeeklyBest.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const WeeklyBest = () => {
+  return (
+    <div className="pt-5">
+      <h1 className="en mt-6 mb-2 text-center">WEEKLY BEST</h1>
+      <p className="text-center pb-5 mb-5">주간 인기상품을 한 눈에 볼 수 있습니다.</p>
+    </div>
+  )
+}
+
+export default WeeklyBest
